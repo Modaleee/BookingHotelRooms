337744d8eb7f7aa9dd0aecbd99d04f0c2d74ed16
Croitoru Madalin <44529195+Modalee@users.noreply.github.com>
2023-10-23 12:57:05 +0300

    Add project files.

---
+++ Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingHotelRooms.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public int Description { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+}
---
+++ Repositories/IRoomRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookingHotelRooms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingHotelRooms.Repositories
+{
+    public class IRoomRepository
+    {
+        Room AddRoom(Room room);
+    }
+}
